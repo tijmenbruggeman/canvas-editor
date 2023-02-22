246530bf376d4280b361af5f2a10b2747f1d5078
tijmen <tijmenbruggeman@gmail.com>
2023-02-22 22:00:04 +0100

    Improve toolbar

---
+++ src/ToolbarAddFile.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+import AddPhotoAlternate from "./icons/add_photo_alternate.svg?component";
+import { addElement } from "./storeEdits";
+import { artboards } from "./storeWorkspace";
+
+let input;
+function onUpload() {
+  const file = input.files[0];
+  const reader = new FileReader();
+  reader.addEventListener("load", function () {
+    const image: any = new Image();
+    image.src = reader.result;
+    image.onload = function () {
+      const { width, height } = this;
+      addElement({
+        type: "img",
+        artboard: $artboards[0].id,
+        src: image.src,
+        height,
+        width,
+      });
+    };
+  });
+  reader.readAsDataURL(file);
+}
+</script>
+
+
+<label class="toolbar-button" for="addfile">
+  <AddPhotoAlternate width="24" height="24" />
+  <span class="toolbar-button-text">Afbeelding</span>
+</label>
+<div class="toolbar-addfile">
+  <input
+    bind:this="{input}"
+    id="addfile"
+    on:change="{onUpload}"
+    accept="image/png, image/jpeg"
+    class="toolbar-addfile-input"
+    type="file" />
+</div>
---
+++ src/ToolbarDownload.svelte
@@ -0,0 +1,8 @@
+<button class="toolbar-button" on:click="{downloadFile}">
+  <Download width="24" height="24" />
+  <span class="toolbar-button-text">Download</span>
+</button>
+<script>
+import Download from "./icons/download.svg?component";
+import { downloadFile } from "./storeWorkspace";
+</script>
